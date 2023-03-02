94b8927bdf570ede3fa789bbb431e726bb090a9d
kohei-kp <cowhey1991@gmail.com>
2023-03-02 10:17:45 +0900

    WezTermのPane系のキーバインドをAlacritty + Tmuxで使っていたキーバインドに近づける

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -5,24 +5,46 @@ wezterm.on('gui-startup', function(cmd)
     wezterm.log_info("gui-startup: %s", cmd)
     local tab, pane, window = mux.spawn_window(cmd or {})
     window:gui_window():set_position(200, 200)
-    window:gui_window():set_inner_size(1280, 640)
+    window:gui_window():set_inner_size(2560, 1280)
     pane:split { size = 0.7, direction = "Top" }
     pane:split { size = 0.5, direction = "Right" }
-end)
-wezterm.on('mux-startup', function(tab)
-    wezterm.log_info("mux-startup: %s", tab)
 end)
 
 return {
     font = wezterm.font_with_fallback({
         {family="HackGen Console NFJ", weight="Regular", stretch="Normal", style="Normal", italic=false}
     }),
-    -- color_scheme = "EverforestDark (Gogh)",
     color_scheme = "iceberg-dark",
-    -- color_scheme = "Banana Blueberry"
-    -- hide_tab_bar_if_only_one_tab = true,
     adjust_window_size_when_changing_font_size = false,
     use_ime = true,
     default_cursor_style = "SteadyBar",
-    force_reverse_video_cursor = true
+    force_reverse_video_cursor = false,
+    leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
+    keys = {
+        {
+            key = "|",
+            mods = "LEADER|SHIFT",
+            action = wezterm.action.SplitHorizontal { domain = "CurrentPaneDomain" },
+        },
+        {
+            key = "-",
+            mods = "LEADER",
+            action = wezterm.action.SplitVertical { domain = "CurrentPaneDomain" },
+        },
+        {
+            key = "x",
+            mods = "LEADER",
+            action = wezterm.action.CloseCurrentPane { confirm = false },
+        },
+        {
+            key = "a",
+            mods = "LEADER|CTRL",
+            action = wezterm.action.SendString "\x01",
+        },
+        {
+            key = "w",
+            mods = "LEADER",
+            action = wezterm.action.PaneSelect
+        },
+    }
 }
